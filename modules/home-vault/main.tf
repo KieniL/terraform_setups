32e8a5a6be42195ce71cbaff8c17d02c53e31bea
Kreidl <lukaskienast0@gmail.com>
2021-12-15 19:00:06 +0100

    added example aws secret backend role

--- modules/home-vault/main.tf
+++ modules/home-vault/main.tf
@@ -1,20 +1,37 @@
 provider "vault" {
 }
 
-resource "vault_auth_backend" "aws" {
-  type = "aws"
-
-  tune {
-    max_lease_ttl      = "90000s"
-  }
-}
-
 resource "vault_policy" "aws-admin-policy" {
   name = "aws-admin"
 
   policy = <<EOT
 path "aws/*" {
   capabilities = ["update","list","read","create"]
+}
+EOT
+}
+
+resource "vault_aws_secret_backend" "aws" {
+  access_key = var.accesskey
+  secret_key = var.secretkey
+
+}
+
+resource "vault_aws_secret_backend_role" "role" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "deploy"
+  credential_type = "iam_user"
+
+  policy_document = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "iam:*",
+      "Resource": "*"
+    }
+  ]
 }
 EOT
 }
---
+++ modules/home-vault/variables.tf
@@ -0,0 +1,9 @@
+variable "accesskey" {
+  type        = string
+  description = "The aws access key"
+}
+
+variable "secretkey" {
+  type        = string
+  description = "The aws secret key"
+}
