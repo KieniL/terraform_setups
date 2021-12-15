5e00d587f99fac163d718ae7ac87a9a016d8fb5b
Kreidl <lukaskienast0@gmail.com>
2021-12-15 18:20:41 +0100

    added aws backend

--- modules/home-vault/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "http" {
-    address = "http://myrest.api.com/foo"
-    lock_address = "http://myrest.api.com/foo"
-    unlock_address = "http://myrest.api.com/foo"
-  }
-}
--- modules/home-vault/main.tf
+++ modules/home-vault/main.tf
@@ -1,4 +1,20 @@
-resource "vault_auth_backend" "vault" {
-  type = "vault"
+provider "vault" {
+}
 
+resource "vault_auth_backend" "aws" {
+  type = "aws"
+
+  tune {
+    max_lease_ttl      = "90000s"
+  }
+}
+
+resource "vault_policy" "aws-admin-policy" {
+  name = "aws-admin"
+
+  policy = <<EOT
+path "aws/*" {
+  capabilities = ["update","list","read","create"]
+}
+EOT
 }
