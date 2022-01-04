227407afc3c6eb74b4932e8cfabebc9019da1d9d
Kreidl <lukaskienast0@gmail.com>
2022-01-04 18:23:03 +0100

    changed output on task04

--- azure/az-900-prep/task-04/output.tf
+++ azure/az-900-prep/task-04/output.tf
@@ -1,6 +1,27 @@
+data "azurerm_public_ip" "public_ip01" {
+  name                = "${var.resource.prefix}-publicip01"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  depends_on = [
+    azurerm_windows_virtual_machine.vm01,
+    azurerm_network_interface.nic01,
+    azurerm_public_ip.public_ip01
+  ]
+}
+
+data "azurerm_public_ip" "public_ip02" {
+  name                = "${var.resource.prefix}-publicip02"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  depends_on = [
+    azurerm_windows_virtual_machine.vm02,
+    azurerm_network_interface.nic02,
+    azurerm_public_ip.public_ip02
+  ]
+}
 
 output "rdp_connection_string_01" {
-  value       = "mstsc.exe /v:${azurerm_public_ip.public_ip01.ip_address}:3389"
+  value       = "mstsc.exe /v:${data.azurerm_public_ip.public_ip01.ip_address}:3389"
   description = "The command for mstsc to the first vm"
 
   depends_on = [
@@ -23,7 +44,7 @@ output "local_win_credentials_01" {
 
 
 output "rdp_connection_string_02" {
-  value       = "mstsc.exe /v:${azurerm_public_ip.public_ip02.ip_address}:3389"
+  value       = "mstsc.exe /v:${data.azurerm_public_ip.public_ip02.ip_address}:3389"
   description = "The command for mstsc to the second vm"
 
   depends_on = [
