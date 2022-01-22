c6a9972c2f29d16995bde7520b1feb057cd088f5
Kreidl <lukaskienast0@gmail.com>
2022-01-22 09:50:39 +0100

    renamed folders

--- applications/aws-policies/main.tf
+++ applications/aws-policies/main.tf
@@ -30,3 +30,8 @@ CONTENT
     project = var.project
   }
 }
+
+resource "aws_organizations_policy_attachment" "unit" {
+  policy_id = aws_organizations_policy.requiretag.id
+  target_id = aws_organizations_organizational_unit.example.id
+}
