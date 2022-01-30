7f0846b359fa8dc62353391afb4f1f48dd8d310a
Kreidl <lukaskienast0@gmail.com>
2022-01-30 06:54:17 +0100

    corrected outputs

--- applications/azure-webserver-scaled-lb/outputs.tf
+++ applications/azure-webserver-scaled-lb/outputs.tf
@@ -1,7 +1,11 @@
 output "gateway_ip" {
-  value = module.appgateway.gateway_ip.ip_address
+  value = module.appgateway.gateway_ip
 }
 
-output "vnet" {
-  value = module.vnet.vnet
+output "vnet_name" {
+  value = module.vnet.vnet.name
+}
+
+output "vnet_resourcegroup" {
+  value = module.vnet.vnet.resource_group_name
 }
