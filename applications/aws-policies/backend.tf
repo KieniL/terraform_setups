851eae7cce2dfdd1bb0d53ab98458b4cff9bb097
Kreidl <lukaskienast0@gmail.com>
2022-02-11 19:15:02 +0100

    removed testing remote backedn

--- applications/aws-policies/backend.tf
+++ applications/aws-policies/backend.tf
@@ -1,9 +1,15 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "tfstate-utirtszb"
+#     key            = "awspolicy.tfstate"
+#     region         = "eu-central-1"
+#     encrypt        = true
+#     dynamodb_table = "tfstatelock-utirtszb"
+#   }
+# }
+
 terraform {
-  backend "s3" {
-    bucket         = "tfstate-utirtszb"
-    key            = "awspolicy.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
-    dynamodb_table = "tfstatelock-utirtszb"
+  backend "local"{
+    
   }
 }
