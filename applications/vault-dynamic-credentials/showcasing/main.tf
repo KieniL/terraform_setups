e4ee6116f5fbe3dbb0d9d35e0c806b8947a6f91b
KienastL <lukaskienast0@gmail.com>
2022-04-29 21:03:14 +0200

    Addedm Admin Role

---
+++ applications/vault-dynamic-credentials/showcasing/main.tf
@@ -0,0 +1,17 @@
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+resource "random_string" "bucketname" {
+  length    = 8
+  special   = false
+  lower     = true
+  min_lower = 8 #force all characters to lowercase
+}
+
+
+resource "aws_s3_bucket" "statestorage" {
+  bucket = "tmp-${var.random_string.bucketname.result}"
+}
